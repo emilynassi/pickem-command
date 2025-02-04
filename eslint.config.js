12d5a96eb7d9c64bfd642d599e2988f9362613fb
Emily Rosenkranz <emily@Emilys-MacBook-Air.local>
2025-02-04 15:43:46 -0500

    fix configs

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      // Add custom rules here
+    },
+  },
+];
